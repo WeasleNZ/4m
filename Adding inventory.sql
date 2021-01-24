add2d0a728d0f4b62a1db0229a28321194e27afd
WeasleNZ <77423101+WeasleNZ@users.noreply.github.com>
2021-01-25 12:50:15 +1300

    Update Adding inventory.sql

--- Adding inventory.sql
+++ Adding inventory.sql
@@ -1,16 +1,20 @@
-DROP TABLE IF EXISTS Inventory, Supplies, Sales, TavernServices, Services, Status, BasementRats, UserRoles, Taverns, Locations, Users, Roles;
+DROP TABLE IF EXISTS Supplysales, GuestClasses, Classes, Guests, Inventory, Supplies, ServiceSales, TavernServices, Services, Status, BasementRats, UserRoles, Taverns, Locations, Users, Roles;
 
 create table Taverns(ID INT IDENTITY(1, 1), Name varchar(250), Floors int, OwnerID int, LocationID int);
 
 create table Users(ID INT IDENTITY(1, 1), Name varchar(250), Birthday datetime);
 
+create table Guests(ID INT IDENTITY(1, 1), Name varchar(250), Notes varchar(250), Birthday datetime, Cakedays varchar(250), StatusID tinyint);
+
 Create table UserRoles(UserID int, RoleID tinyint);
+
+Create table GuestClasses(GuestID int, ClassID int, Level int);
+
+Create table Classes(ID int IDENTITY(1,1), Name varchar(250));
 
 create table Roles(ID tinyint identity(1,1), Name varchar(50), Description varchar(MAX));
 
 Create table Locations (ID int identity(1,1), Name varchar(100));
-
-create table BasementRats (ID int IDENTITY (1,1), Name varchar(100), TavernID int);
 
 Create table Inventory(TavernID int, SupplyID int, Quantity int, DateReceived DATETIME);
 
@@ -18,7 +22,9 @@ Create table Supplies(ID int identity (1,1), Name varchar(250), Unit varchar(100
 
 Create TABLE TavernServices(TavernID int, ServiceID int, StatusID Tinyint);
 
-Create TABLE Sales(ID int identity(1,1), ServiceID int, UsersID int, Price decimal, DatePurchased DATETIME, TavernID int);
+Create TABLE ServiceSales(ID int identity(1,1), ServiceID int, UsersID int, Price decimal, DatePurchased DATETIME, TavernID int);
+
+Create TABLE SupplySales(ID int identity(1,1), SupplyID int, GuestID int, Price decimal, DatePurchased DATETIME, TavernID int);
 
 CREATE TABLE Services(ID int IDENTITY(1,1), Name VARCHAR(250));
 
@@ -28,20 +34,28 @@ ALTER TABLE Locations ADD primary key (id);
 ALTER TABLE Users ADD primary key (id);
 ALTER TABLE Roles ADD primary key (id);
 ALTER TABLE Services ADD primary key (ID);
-ALTER TABLE Sales ADD primary key (ID);
+ALTER TABLE ServiceSales ADD primary key (ID);
+ALTER TABLE SupplySales ADD primary key (ID);
 ALTER TABLE Supplies ADD primary key (ID);
 ALTER TABLE Status ADD primary key (ID);
 ALTER TABLE Taverns ADD primary key (ID);
+ALTER TABLE Guests ADD primary key (ID);
+ALTER TABLE Classes ADD primary key (ID);
 ALTER TABLE Taverns ADD FOREIGN KEY (LocationID) References Locations(ID);
 ALTER TABLE Taverns ADD FOREIGN KEY (OwnerID) References Users(ID);
 ALTER TABLE UserRoles ADD FOREIGN KEY (UserID) References Users(ID);
 ALTER TABLE UserRoles ADD FOREIGN KEY (RoleID) References Roles(ID);
-ALTER TABLE BasementRats ADD FOREIGN KEY (TavernID) References Taverns(ID);
 ALTER TABLE Inventory ADD FOREIGN KEY (TavernID) References Taverns(ID);
 ALTER TABLE Inventory ADD FOREIGN KEY (SupplyID) References Supplies(ID);
 ALTER TABLE TavernServices ADD FOREIGN KEY (TavernID) References Taverns(ID);
 ALTER TABLE TavernServices ADD FOREIGN KEY (ServiceID) References Services(ID);
 ALTER TABLE TavernServices ADD FOREIGN KEY (StatusID) References Status(ID);
-ALTER TABLE Sales ADD FOREIGN Key (ServiceID) REFERENCES Services(ID);
-ALTER TABLE Sales ADD FOREIGN Key (UsersID) REFERENCES Users(ID);
-ALTER TABLE Sales ADD FOREIGN Key (TavernID) REFERENCES Taverns(ID);
+ALTER TABLE ServiceSales ADD FOREIGN Key (ServiceID) REFERENCES Services(ID);
+ALTER TABLE ServiceSales ADD FOREIGN Key (UsersID) REFERENCES Users(ID);
+ALTER TABLE ServiceSales ADD FOREIGN Key (TavernID) REFERENCES Taverns(ID);
+ALTER TABLE SupplySales ADD FOREIGN Key (SupplyID) REFERENCES Supplies(ID);
+ALTER TABLE SupplySales ADD FOREIGN Key (GuestID) REFERENCES Guests(ID);
+ALTER TABLE SupplySales ADD FOREIGN Key (TavernID) REFERENCES Taverns(ID);
+ALTER TABLE GuestClasses ADD FOREIGN Key (GuestID) REFERENCES Guests(ID);
+ALTER TABLE GuestClasses ADD FOREIGN Key (ClassID) REFERENCES Classes(ID);
+ALTER TABLE Guests ADD FOREIGN Key (StatusID) REFERENCES Status(ID);
